cf80b8edc991615b32dbe0ddd7c92e72dc2f5672
shereali2989 <shereali2989@gmail.com>
2019-07-31 15:20:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const verifyWebhook = require('./verify-webhook');
+const express = require('express');
+const bodyParser = require('body-parser');
+const verifyWebhook = require('./verify-webhook');
 
-// const app = express();
+const app = express();
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.get('/', (req, res) => res.send('Hello World!'))
-// app.listen(3000, () => console.log('Express server is listening on port 3000'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3000, () => console.log('Express server is listening on port 3000'));
 
-// app.get('/', verifyWebhook);
+app.get('/', verifyWebhook);
 
-console.log("hi");
+// console.log("hi");
