f5ac847547e998858654ec0e5c9bce9ce4e52818
Valimohammed Rajabali <shere-ali.valimohammed-rajabali@capgemini.com>
2019-07-31 12:49:57 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const verifyWebhook = require('./verify-webhook');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3000, () => console.log('Express server is listening on port 3000'));
+
+app.get('/', verifyWebhook);
