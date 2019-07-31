2abb547ce8d8d8341fd56b66a771e22f68fe8296
shereali2989 <shereali2989@gmail.com>
2019-07-31 15:23:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Express server is listening on port 3000'));
+app.listen(process.env.PORT || 5000, () => console.log('Express server is listening on port 3000'));
 
 app.get('/', verifyWebhook);
 
